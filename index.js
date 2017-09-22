a428ae357e081601f4683e38e5fac7f73b34f12c
Perry Fraser <perry@frasers.org>
2017-09-22 10:06:50 -0400

    Use own version of emoticons, make code mostly client side.

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/cool', function(request, response) {
-  response.render('pages/index');
-});
+// app.get('/cool', function(request, response) {
+//   response.render('pages/index');
+// });
 
 app.get('/', function (request, response) {
-  response.render("pages/cool", { emote: cool() });
+  response.render("pages/cool", { emote: cool(), emotes: cool.faces });
 })
 
 app.listen(app.get('port'), function() {
