044c0663ca20aaa90cc69177c554cbcb2e324ef6
Perry Fraser <perry@frasers.org>
2017-09-21 20:37:28 -0400

    Update versions, add ascii faces and more

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var cool = require('cool-ascii-faces');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,9 +10,13 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.get('/cool', function(request, response) {
   response.render('pages/index');
 });
+
+app.get('/', function (request, response) {
+  response.render("pages/cool", { emote: cool() });
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
